1d879e279f7a8c6404e96ebc23e8daa832540148
Wesley Mota Araujo <wesley.ma@varsitytutors.com>
2022-06-14 14:40:19 -0300

    adding logs

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,17 @@ async function run(): Promise<void> {
         const octokit = github.getOctokit(inputs.token);
         const repository: string = process.env.GITHUB_REPOSITORY as string;
         const [owner, repo] = repository.split("/");
+        
+        core.info(`owner: ${owner}`);
+        core.info(`repo: ${repo}`);
+        core.info(`branch: ${inputs.branch}`);
+        core.info(`workflow: ${inputs.workflow}`);
+        core.info(`verify: ${inputs.verify}`);
 
         const workflows = await octokit.actions.listRepoWorkflows({ owner, repo });
+        core.info(`workflows: ${JSON.stringify(workflows)}`);
         const workflowId = workflows.data.workflows.find(w => w.name === inputs.workflow)?.id;
+        core.info(`workflowId: ${workflowId}`);
 
         if (!workflowId) {
             core.setFailed(`No workflow exists with the name "${inputs.workflow}"`);
@@ -24,7 +32,9 @@ async function run(): Promise<void> {
             core.info(`Discovered workflowId for search: ${workflowId}`);
         }
 
-        const response = await octokit.actions.listWorkflowRuns({ owner, repo, workflow_id: workflowId, per_page: 100 });
+        const response = await octokit.actions.listWorkflowRuns({ owner, repo, workflow_id: workflowId, per_page: 500 });
+        core.info(`response: ${JSON.stringify(response)}`);
+        
         const runs = response.data.workflow_runs
             .filter(x => (!inputs.branch || x.head_branch === inputs.branch) && x.conclusion === "success")
             .sort((r1, r2) => new Date(r2.created_at).getTime() - new Date(r1.created_at).getTime());
